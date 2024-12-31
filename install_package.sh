e9991e05d07a5d44a1c17c83f21f86b15a750b8e
1304SHUBHAM <134464746+1304SHUBHAM@users.noreply.github.com>
2024-12-31 12:14:05 +0530

    Create install_package.sh

---
+++ install_package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+<<disclaimer
+is kahani k sabhi patr or ghatnaye kalpnik hai
+disclaimer
+
+read -p "enter gabbar dilogue:" gb
+
+read -p "enter thakur dilogue:" th
+
+echo "$gb"
+echo "$th"
+
+if [[ $th == "nhi" ]];
+then
+        echo "jai veeru ki entry aur bhasad"
+
+        echo "chop chop"
+fi
+
+echo "sholay khatam"
+~                        
